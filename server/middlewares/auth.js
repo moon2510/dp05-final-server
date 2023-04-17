dbee5f40e81198d5ef55d496b65d72d2be621c7e
nguyendeptrai <nguyendeptrai@gmail.com>
2023-04-17 22:50:19 +0700

    feat: authorization-middleware-refreshtoken-and-api-profile

---
+++ server/middlewares/auth.js
@@ -0,0 +1,24 @@
+const UserSchema = require('../models/user.model')
+const jsonwebtoken = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header("Authorization")
+        
+        if(!token) return res.status(400).json({msg: "Invalid Authentication."})
+
+        const decoded = jsonwebtoken.verify(token, "DP05")
+        if(!decoded) return res.status(400).json({msg: "Invalid Authentication."})
+
+        const user = await UserSchema.findOne({_id: decoded.token.id})
+        if(!user) return res.status(400).json({msg: "Invalid Authentication."})
+        
+        req.user = user
+        next()
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
+}
+
+
+module.exports = auth

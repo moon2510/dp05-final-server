116c09d6c5f0ce98421e92ff7ec0c0cab88c848c
Nguyen Dang Hung <hungnguyendang2000@gmail.com>
2023-04-17 09:59:50 +0700

    feat: add-models-and-constants-error

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,9 @@
-const Users = require("../models/user.model");
-require("dotenv").config();
+const Users = require('../models/user.model');
+require('dotenv').config();
 // const Payments = require('../models/paymentModel')
 
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const UserController = {
   register: async (req, res) => {
@@ -13,8 +13,8 @@ const UserController = {
 
       const user = await Users.findOne({ email });
       if (user)
-        return res.status(400).json({ msg: "The email already exists." });
-      let passwordHash = await bcrypt.hash("123456", 10);
+        return res.status(400).json({ msg: 'The email already exists.' });
+      let passwordHash = await bcrypt.hash('123456', 10);
 
       const newUser = new Users({
         fullName,
@@ -30,7 +30,7 @@ const UserController = {
       // Save mongodb
       await newUser.save();
 
-      res.json({ msg: "Create User Successfully" });
+      res.json({ msg: 'Create User Successfully' });
     } catch (error) {
       console.log(error);
     }
@@ -41,12 +41,12 @@ const UserController = {
       const { email, password } = req.body;
 
       const user = await Users.findOne({ email });
-      if (!user) return res.status(400).json({ msg: "User does not exist." });
+      if (!user) return res.status(400).json({ msg: 'User does not exist.' });
 
       const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) return res.status(400).json({ msg: "Incorrect password." });
+      if (!isMatch) return res.status(400).json({ msg: 'Incorrect password.' });
 
-      res.json({ msg: "Login Successfully" });
+      res.json({ msg: 'Login Successfully' });
     } catch (error) {
       console.log(error);
     }

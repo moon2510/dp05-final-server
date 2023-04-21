55f23a26a5bae99fc3028e00efa18a000a158373
kiranguyen2000 <98039366+kiranguyen2000@users.noreply.github.com>
2023-04-21 14:30:20 +0700

    feat: crud-workspace-manager (#14)
    * feat: crud-manager
    
    * feat: crud-workspace

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -1,18 +1,66 @@
 const Users = require('../models/user.model');
+const WorkSpace = require('../models/workSpace.model');
 require('dotenv').config();
+const bcrypt = require('bcryptjs');
 
 const AdminController = {
   createManager: async (req, res) => {
     try {
-      const { fullName, phone, email, staffId, avatar, groupsId, role } =
-        req.body;
-
+      const { fullName, phone, email, avatar, slackId, groupsId } = req.body;
       const user = await Users.findOne({ email });
       if (user)
         return res.status(400).json({ msg: 'The email already exists.' });
       let passwordHash = await bcrypt.hash('123456', 10);
-
       const newUser = new Users({
+        fullName,
+        phone,
+        email,
+        avatar,
+        slackId,
+        groupsId,
+        role: 'Manager',
+        password: passwordHash,
+      });
+
+      // Save mongodb
+      await newUser.save();
+      res.json({ msg: 'Create User Successfully' });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  getListManager: async (req, res) => {
+    try {
+      const managers = await Users.find({ role: 'Manager' });
+      res.send(managers);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  deleteManager: async (req, res) => {
+    try {
+      const exist_manager = await Users.findById(req.params.id);
+      if (!exist_manager)
+        return res.status(400).json({ msg: 'The manager doesnt exists.' });
+
+      await Users.findOneAndDelete({ _id: req.params.id });
+      res.json({ message: 'Manager deleted successfully.' });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  updateManager: async (req, res) => {
+    try {
+      const { fullName, phone, email, staffId, avatar, groupsId, role } =
+        req.body;
+
+      const exist_manager = await Users.findById(req.params.id);
+      if (!exist_manager)
+        return res.status(400).json({ msg: 'The manager doesnt exists.' });
+
+      let passwordHash = await bcrypt.hash('123456', 10);
+
+      const update_manager = {
         fullName,
         phone,
         email,
@@ -21,22 +69,103 @@ const AdminController = {
         groupsId,
         role,
         password: passwordHash,
-      });
+      };
+      await Users.findByIdAndUpdate({ _id: req.params.id }, update_manager);
 
-      // Save mongodb
-      await newUser.save();
-
-      res.json({ msg: 'Create User Successfully' });
+      res.json({ message: 'Manager Updated successfully.' });
     } catch (error) {
       console.log(error);
     }
   },
-  getListManager: async (req, res) => {
+  getOneWorkSpace: async (req, res) => {
     try {
-      const managers = await Users.find({ role: 'Manager' }).sort({
-        fullName: 1,
+      const WorkSpaces = await WorkSpace.findById(req.params.id)
+        .populate({ path: hrChannel })
+        .populate({ path: dayOffChannel });
+      res.send(WorkSpaces);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  getWorkSpace: async (req, res) => {
+    try {
+      const workspaces = await WorkSpace.find();
+      // .populate({
+      //   path: 'dayOffChannel',
+      // });
+      res.status(200).json(workspaces);
+      console.log(workspaces);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  // getWorkSpace: async (req, res) => {
+  //   try {
+  //     const workspace = await WorkSpace.find({ role: 'Manager' }).populate({
+  //       path: 'managers',
+  //     });
+  //     // .populate({
+  //     //   path: 'dayOffChannel',
+  //     // });
+  //     res.status(200).json(workspace);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // },
+  createWorkSpace: async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      const exist_workspace = await WorkSpace.findOne({ name });
+      if (exist_workspace)
+        return res.status(400).json({ msg: 'The workspace already exists.' });
+
+      const new_workspace = new WorkSpace({
+        name,
+        hrChannel: [],
+        dayOffChannel: [],
+        managers: [],
       });
-      res.send(managers);
+
+      await new_workspace.save();
+
+      res.json({ msg: 'Create Workspace Successfully' });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  deleteWorkSpace: async (req, res) => {
+    try {
+      const exist_workspace = await WorkSpace.findById(req.params.id);
+      if (!exist_workspace)
+        return res.status(400).json({ msg: 'The workspace doesnt exists.' });
+
+      await WorkSpace.findOneAndDelete({ _id: req.params.id });
+      res.json({ message: 'Workspace deleted successfully.' });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  updateWorkSpace: async (req, res) => {
+    try {
+      const { name, managers } = req.body;
+
+      const exist_workspace = await WorkSpace.findById(req.params.id);
+      if (!exist_workspace)
+        return res.status(400).json({ msg: 'The workspace doesnt exists.' });
+
+      const update_workspace = {
+        name,
+        hrChannel: [],
+        dayOffChannel: [],
+        managers,
+      };
+      await PostMessage.findByIdAndUpdate(
+        { _id: req.params.id },
+        update_workspace
+      );
+
+      res.json({ message: 'Workspace Updated successfully.' });
     } catch (error) {
       console.log(error);
     }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,12 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
 });
-router.use("/admin", require("./user.route"));
-router.use("/profile", require("./profile.route"));
-router.use("/manager", require("./manager.route"));
+router.use('/admin', require('./admin.route'));
+router.use('/profile', require('./profile.route'));
+router.use('/manager', require('./manager.route'));
+router.use('/user', require('./user.route'));
 module.exports = router;

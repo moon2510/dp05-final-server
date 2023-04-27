6affc8c87b2635b7195cd4d815ac87af51a5e922
ltkien <66240745+ltkien@users.noreply.github.com>
2023-04-27 14:40:05 +0700

    fear: Create-api-list-group (#17)

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -9,6 +9,7 @@ router.get("/", function (req, res, next) {
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
-router.post('/refresh_token', UserController.getRefreshToken)
+router.post("/refresh_token", UserController.getRefreshToken);
+router.get("/getUserGroup", UserController.getUserGroups);
 
 module.exports = router;
